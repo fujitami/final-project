f21c3fcb10d45f03c88552cb3eea32023a23b0b9
fujitami <midori.fujita.0423@gmail.com>
2018-08-29 23:49:47 +0900

    Update config
    railsおよびdeviseの日本語化対応
    プリコンパイルの設定

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module FinalProject
       g.javascripts false
       g.helper false
     end
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
