1e922fa600493c564f5a7d0e68160d3a3d1acde3
fujitami <midori.fujita.0423@gmail.com>
2018-09-18 23:33:05 +0900

    Update artists_controller
    不要な記述の削除

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,9 +2,5 @@ class ArtistsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    respond_to do |format|
-      format.html
-      format.json
-    end
   end
 end
