cc6eb2b1031e4955bf0c578c1c01a4fe2937a265
fujitami <midori.fujita.0423@gmail.com>
2018-10-01 03:56:56 +0900

    Add controller and service for artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,4 +3,15 @@ class ArtistsController < ApplicationController
 
   def show
   end
+
+  # アルバムに最も多くついたタグのidを抽出
+  def tagged_search
+    album_id = params[:id]
+    @most_tagged_tag_id = ArtistsTaggedSearchService.new(album_id).get_most_tagged_tag_id
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
 end
