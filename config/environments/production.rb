7cd83f91b0d85d99c673fa3faf474694ba7229dc
fujitami <midori.fujita.0423@gmail.com>
2018-10-04 08:36:48 +0900

    Update setting of deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
     s3_region: 'ap-northeast-1',
     s3_host_name: 's3-ap-northeast-1.amazonaws.com',
     s3_credentials: {
-      access_key_id: ENV['AWS_ACCESS_KEY'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
