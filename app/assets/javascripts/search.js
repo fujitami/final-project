179519c636223076c1f483dab820430ca99efc85
fujitami <midori.fujita.0423@gmail.com>
2018-10-19 02:14:05 +0900

    Revert "Update search.js"
    This reverts commit 6d9dcba79f76a71b151d63d18a3df20be3cb1875.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -28,8 +28,7 @@ $(function () {
     var base64IdAndSecret = window.btoa(clientIdAndSecretVar);
     var accessToken = '';
     var refreshTokenVar = secrets.refreshToken;
-    // const proxyurl = 'https://cors-anywhere.herokuapp.com/';
-    const proxyurl = 'http://178.128.48.112:60088/';
+    const proxyurl = 'https://cors-anywhere.herokuapp.com/';
     const requireTokenUrl = 'https://accounts.spotify.com/api/token';
 
     // 検索機能のための変数定義
